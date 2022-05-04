ac5ce0866e2707d8498014e9a29e8e5d08873c15
Rene Ortega <reneortega2468@gmail.com>
2022-05-04 09:21:54 -0400

    models done, working on routes!

---
+++ models/Tag.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+        allowNull: false,
+          primaryKey: true,
+            autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+        reference : {
+          model: 'product',
+            key: 'id',
+        },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+        reference: {
+          model: 'tag',
+            key: 'id',
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
