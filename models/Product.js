0e286960a40b8a4c5911f0f887c550956237fcca
Rene Ortega <95727555+Rini0404@users.noreply.github.com>
2022-05-04 15:51:40 -0400

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,8 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
     // define columns
@@ -16,6 +12,7 @@ Product.init(
           autoIncrement: true,
             primaryKey: true,
     },
+     // define columns
     price: {
       type: DataTypes.DECIMAL,
         allowNull: false,
@@ -23,16 +20,18 @@ Product.init(
             isDecimal: true,
           }
     },
+     // define columns
     product_name: {
       type: DataTypes.STRING,
         allowNull: false,
     },
-    
+     // define columns
     stock: {
       type: DataTypes.INTEGER,
         allowNull: false,
           defaultValue: 10,    
     }, 
+     // define columns
     category_id: {
               type: DataTypes.INTEGER,
                 reference: {
