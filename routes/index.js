7745cd4a352ddaa1b5e878b1ba97cb4821eb1b6c
Rene Ortega <95727555+Rini0404@users.noreply.github.com>
2022-05-04 15:47:15 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
 router.use('/api', apiRoutes);
-
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("Bad path...)
 });
 
 module.exports = router;
